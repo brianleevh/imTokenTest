f3972c16b9b676699fc7c5aa933d85858b90f095
Brian Lee <vernhuilee@gmail.com>
2023-02-28 01:07:12 +0800

    Fixed network module output error

--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -7,3 +7,18 @@ output "private_subnet_id" {
   value = google_compute_subnetwork.priv_subnet.id
   description = "Private Subnet ID"
 }
+
+output "network_self_link" {
+  value = google_compute_network.vpc_network.self_link
+  description = "VPC Network Self_link"
+}
+
+output "public_subnet_self_link" {
+  value = google_compute_subnetwork.pub_subnet.self_link
+  description = "Public Subnetwork Self_link"
+}
+
+output "private_subnet_self_link" {
+  value = google_compute_subnetwork.priv_subnet.self_link
+  description = "Private Subnetwork Self_link"
+}
