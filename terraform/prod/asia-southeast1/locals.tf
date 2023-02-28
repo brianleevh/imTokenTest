215fd449777a9fbf67cf8f13b008e81760b44232
Brian Lee <vernhuilee@gmail.com>
2023-03-01 03:36:32 +0800

    Added prod env

---
+++ terraform/prod/asia-southeast1/locals.tf
@@ -0,0 +1,12 @@
+locals {
+  gcp_project  = "artful-athlete-379106"
+  environment  = "prod"
+  region       = "asia-southeast1"
+  region_abrv  = "sea1"
+  zone_1       = "asia-southeast1-a"
+  zone_2       = "asia-southeast1-b"
+  zone_3       = "asia-southeast1-c"
+  zone_4       = "asia-southeast1-d"
+  project_name = "imtoken"
+  prefix       = "${local.project_name}-${local.environment}-${local.region_abrv}"
+}
